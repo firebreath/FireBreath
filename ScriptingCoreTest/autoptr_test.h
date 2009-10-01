c61b52fb80233bbc1876d760361106ce33f2e7e6
kc7zax@gmail.com <kc7zax@gmail.com>
2009-10-01 15:43:21 -0600

    More unit tests

--- ScriptingCoreTest/autoptr_test.h
+++ ScriptingCoreTest/autoptr_test.h
@@ -7,7 +7,17 @@ class TestObject
 
 		TestObject() : refCount(0) {}
 		void addRef() { refCount++; }
-		int release() { refCount--; return refCount; }
+		int release()
+    {
+        refCount--;
+        if (refCount == 0)
+        {
+            delete this;
+            return 0;
+        }
+
+        return refCount;
+    }
 		bool AccessMe() { return true; }
 };
 
@@ -15,13 +25,21 @@ TEST (AutoPtrTest)
 {
   FB::AutoPtr<TestObject> ptr;
 	FB::AutoPtr<TestObject> ptr2;
+  FB::AutoPtr<TestObject> ptr3;
 	ptr = new TestObject;
+  ptr3 = new TestObject;
 
 	CHECK (ptr->refCount == 1);
 	ptr2 = ptr;
 	CHECK (ptr->refCount == 2);
 	ptr2 = NULL;
 	CHECK (ptr->refCount == 1);
+
+  CHECK (ptr3->refCount == 1);
+  ptr2 = ptr;
+  ptr = ptr3;
+  CHECK (ptr3->refCount == 2);
+  CHECK (ptr2->refCount == 1);
 
 	CHECK ((*ptr).AccessMe() == true);
 	CHECK (ptr->AccessMe() == true);
