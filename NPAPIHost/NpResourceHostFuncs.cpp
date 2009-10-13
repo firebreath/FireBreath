81387ad99cd5d4ae43a2cf0c830b84ae426ee66e
Richard Bateman <taxilian@gmail.com>
2009-10-13 12:05:02 -0600

    Added basic implementation of memory allocation / freeing methods in the NPAPIHost project

--- NPAPIHost/NpResourceHostFuncs.cpp
+++ NPAPIHost/NpResourceHostFuncs.cpp
@@ -5,36 +5,62 @@
 /* NPN_MemAlloc */
 void* NP_LOADDS NpapiHost_MemAlloc(uint32 size)
 {
+	return malloc(size);
 }
 
 /* NPN__MemFree */
 void NP_LOADDS NpapiHost_MemFree(void* ptr)
 {
+	free(ptr);
 }
 
 /* NPN_MemFlush */
 uint32 NP_LOADDS NpapiHost_MemFlush(uint32 size)
 {
+	return 0;
 }
 
 /* NPN_CreateObject */
+
 NPObject* NP_LOADDS NpapiHost_CreateObject(NPP npp, NPClass *aClass)
 {
+	NPObject *newObj(NULL);
+	if (aClass->allocate) {
+		newObj = aClass->allocate(npp, aClass);
+	} else {
+		newObj = (NPObject *)malloc(sizeof(NPObject));
+	}
+	newObj->referenceCount = 1;
+	newObj->_class = aClass;
+	return newObj;
 }
 
 /* NPN_RetainObject */
 NPObject* NP_LOADDS NpapiHost_RetainObject(NPObject *obj)
 {
+	obj->referenceCount++;
 }
 
 /* NPN_ReleaseObject */
 void NP_LOADDS NpapiHost_ReleaseObject(NPObject *obj)
 {
+	if (--(obj->referenceCount) == 0) {
+		if (obj->_class->deallocate) {
+			obj->_class->deallocate(obj);
+		} else {
+			free(obj);
+		}
+	}
 }
 
 /* NPN_ReleaseVariantValue */
 void NP_LOADDS NpapiHost_ReleaseVariantValue(NPVariant *variant)
 {
+	if (variant->type == NPVariantType_String) {
+		NPN_FreeMem(variant->value.stringValue.utf8characters);
+	} else if (variant->type == NPVariantType_Object) {
+		NPN_ReleaseObject(variant->value.objectValue);
+	}
 }
 
 /* NPN_GetStringIdentifier */
