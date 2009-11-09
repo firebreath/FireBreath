e1ab0d23262d02da00fd80ececc6743d0d9df24c
georg.fritzsche@gmail.com <georg.fritzsche@gmail.com>
2009-11-09 02:27:06 +0100

    Added forgotten legalese to ConverterUtils.h

--- ScriptingCore/ConverterUtils.h
+++ ScriptingCore/ConverterUtils.h
@@ -1,3 +1,12 @@
+/**********************************************************\ 
+Original Author: Georg Fritzsche
+
+Created:    November 9, 2009
+License:    Eclipse Public License - Version 1.0
+            http://www.eclipse.org/legal/epl-v10.html
+
+Copyright 2009 Georg Fritzsche, Firebreath development team
+\**********************************************************/
 
 #if !defined(CONVERTER_UTILS_H)
 #define CONVERTER_UTILS_H
