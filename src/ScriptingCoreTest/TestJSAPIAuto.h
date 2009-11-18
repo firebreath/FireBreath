1fcc6681cea00b65c509f2afefd187784c47ed7e
georg.fritzsche@gmail.com <georg.fritzsche@gmail.com>
2009-11-18 17:56:13 +0100

    Tabs to spaces.

--- src/ScriptingCoreTest/TestJSAPIAuto.h
+++ src/ScriptingCoreTest/TestJSAPIAuto.h
@@ -15,74 +15,74 @@ Copyright 2009 Georg Fritzsche, Firebreath development team
 class TestObjectJSAPIAuto : public JSAPIAuto
 {
 public:
-	virtual ~TestObjectJSAPIAuto() {}
+    virtual ~TestObjectJSAPIAuto() {}
 
-	TestObjectJSAPIAuto()
-	  : m_readOnlyMessage("read only message")
-	{
-		registerMethod("returnString",  make_method(this, &TestObjectJSAPIAuto::returnString));
-		registerMethod("intToString",   make_method(this, &TestObjectJSAPIAuto::intToString));
-		registerMethod("sumOf",         make_method(this, &TestObjectJSAPIAuto::sumOf));
-		registerMethod("concatenate",   make_method(this, &TestObjectJSAPIAuto::concatenate));
-		registerMethod("concatMany",    make_method(this, &TestObjectJSAPIAuto::concatenate2));
-		registerMethod("getType",       make_method(this, &TestObjectJSAPIAuto::getType));
+    TestObjectJSAPIAuto()
+      : m_readOnlyMessage("read only message")
+    {
+        registerMethod("returnString",  make_method(this, &TestObjectJSAPIAuto::returnString));
+        registerMethod("intToString",   make_method(this, &TestObjectJSAPIAuto::intToString));
+        registerMethod("sumOf",         make_method(this, &TestObjectJSAPIAuto::sumOf));
+        registerMethod("concatenate",   make_method(this, &TestObjectJSAPIAuto::concatenate));
+        registerMethod("concatMany",    make_method(this, &TestObjectJSAPIAuto::concatenate2));
+        registerMethod("getType",       make_method(this, &TestObjectJSAPIAuto::getType));
 
-		registerProperty("message",
-						 make_property(this, 
-							&TestObjectJSAPIAuto::get_Message,
-							&TestObjectJSAPIAuto::set_Message));
-		registerProperty("readOnlyMessage", 
-						 make_property(this,
-							&TestObjectJSAPIAuto::get_ReadOnlyMessage));
-	}
+        registerProperty("message",
+                         make_property(this, 
+                            &TestObjectJSAPIAuto::get_Message,
+                            &TestObjectJSAPIAuto::set_Message));
+        registerProperty("readOnlyMessage", 
+                         make_property(this,
+                            &TestObjectJSAPIAuto::get_ReadOnlyMessage));
+    }
 
-	std::string returnString(const std::string& s)
-	{
-		return s;
-	}
+    std::string returnString(const std::string& s)
+    {
+        return s;
+    }
 
-	std::string intToString(int i)
-	{
-		std::stringstream ss;
-		ss << i;
-		return ss.str();
-	}
+    std::string intToString(int i)
+    {
+        std::stringstream ss;
+        ss << i;
+        return ss.str();
+    }
 
-	int sumOf(int a, int b)
-	{
-		return a+b;
-	}
+    int sumOf(int a, int b)
+    {
+        return a+b;
+    }
 
-	std::string concatenate(const std::string& a, const std::string& b, const std::string& c)
-	{
-		return a+b+c;
-	}
+    std::string concatenate(const std::string& a, const std::string& b, const std::string& c)
+    {
+        return a+b+c;
+    }
 
-	std::string concatenate2(const std::string& a, const FB::VariantList& list)
-	{
-		FB::VariantList::const_iterator it(list.begin());
-		std::string s(a);
+    std::string concatenate2(const std::string& a, const FB::VariantList& list)
+    {
+        FB::VariantList::const_iterator it(list.begin());
+        std::string s(a);
 
-		for( ; it != list.end(); ++it)
-			s += it->convert_cast<std::string>();
+        for( ; it != list.end(); ++it)
+            s += it->convert_cast<std::string>();
 
-		return s;
-	}
+        return s;
+    }
 
-	void set_Message(const std::string& s)
-	{
-		m_message = s;
-	}
+    void set_Message(const std::string& s)
+    {
+        m_message = s;
+    }
 
-	std::string get_Message()
-	{
-		return m_message;
-	}
+    std::string get_Message()
+    {
+        return m_message;
+    }
 
-	std::string get_ReadOnlyMessage()
-	{
-		return m_readOnlyMessage;
-	}
+    std::string get_ReadOnlyMessage()
+    {
+        return m_readOnlyMessage;
+    }
 
     std::string getType(const FB::variant a)
     {
@@ -90,6 +90,6 @@ public:
     }
 
 private:
-	std::string m_message;
-	const std::string m_readOnlyMessage;
+    std::string m_message;
+    const std::string m_readOnlyMessage;
 };
--- src/ScriptingCoreTest/jsapiauto_test.h
+++ src/ScriptingCoreTest/jsapiauto_test.h
@@ -10,108 +10,125 @@ Copyright 2009 Georg Fritzsche, Firebreath development team
 
 #include <vector>
 #include <sstream>
+#include <numeric>
 #include <boost/assign.hpp>
+#include <boost/lexical_cast.hpp>
 #include "TestJSAPIAuto.h"
 
 namespace helper
 {
-	template<class T>
-	std::string toString(const T t)
-	{
-		std::stringstream ss;
-		ss << t;
-		return ss.str();
-	}
+    template<class T>
+    std::string toString(const T t)
+    {
+        std::stringstream ss;
+        ss << t;
+        return ss.str();
+    }
 }
 
 TEST(JSAPIAuto_Methods)
 {
-	PRINT_TESTNAME;
+    PRINT_TESTNAME;
 
-	using boost::assign::list_of;
-	FB::AutoPtr<FB::JSAPI> test = new TestObjectJSAPIAuto;
+    using boost::assign::list_of;
+    FB::AutoPtr<FB::JSAPI> test = new TestObjectJSAPIAuto;
 
-	{
-		const std::string method("returnString");
-		CHECK(test->HasMethod(method));
-		const std::string value("foo");
-		FB::VariantList args = list_of(value);		
-		FB::variant ret = test->Invoke(method, args);		
-		CHECK(ret.cast<std::string>() == "foo");
-	}
+    {
+        const std::string method("returnString");
+        CHECK(test->HasMethod(method));
+        const std::string value("foo");
+        FB::VariantList args = list_of(value);        
+        FB::variant ret = test->Invoke(method, args);        
+        CHECK(ret.cast<std::string>() == "foo");
+    }
 
-	{
-		const std::string method("intToString");
-		CHECK(test->HasMethod(method));
-		const int value = 23;
-		FB::VariantList args = list_of(value);		
-		FB::variant ret = test->Invoke(method, args);		
-		CHECK(ret.cast<std::string>() == helper::toString(value));
-	}
+    {
+        const std::string method("intToString");
+        CHECK(test->HasMethod(method));
+        const int value = 23;
+        FB::VariantList args = list_of(value);        
+        FB::variant ret = test->Invoke(method, args);        
+        CHECK(ret.cast<std::string>() == helper::toString(value));
+    }
 
-	{
-		const std::string method("sumOf");
-		CHECK(test->HasMethod(method));
-		int values[2] = {0, 42};
-		for(int i=0; i<5; ++i, ++values[0], --values[1])
-		{
-			FB::VariantList args(values, values + sizeof(values)/sizeof(int));
-			FB::variant ret = test->Invoke(method, args);		
-			CHECK(ret.cast<int>() == (values[0]+values[1]));
-		}
-	}
+    {
+        const std::string method("sumOf");
+        CHECK(test->HasMethod(method));
+        int values[2] = {0, 42};
+        for(int i=0; i<5; ++i, ++values[0], --values[1])
+        {
+            FB::VariantList args(values, values + sizeof(values)/sizeof(int));
+            FB::variant ret = test->Invoke(method, args);        
+            CHECK(ret.cast<int>() == (values[0]+values[1]));
+        }
+    }
 
-	{
-		const std::string method("concatenate");
-		CHECK(test->HasMethod(method));
-		const std::string a("push "), b("me "), c("please");
-		FB::VariantList args = list_of(a)(b)(c);
-		FB::variant ret = test->Invoke(method, args);
-		CHECK(ret.cast<std::string>() == (a+b+c));
-	}
+    {
+        const std::string method("concatenate");
+        CHECK(test->HasMethod(method));
+        const std::string a("push "), b("me "), c("please");
+        FB::VariantList args = list_of(a)(b)(c);
+        FB::variant ret = test->Invoke(method, args);
+        CHECK(ret.cast<std::string>() == (a+b+c));
+    }
 
-	{
-		const std::string method("concatMany");
-		CHECK(test->HasMethod("concatMany"));
-		const std::string a("a"), b("b"), c("c"), d("d");
-		FB::VariantList args = list_of(a)(b)(c)(d);
-		FB::variant ret = test->Invoke(method, args);
-		CHECK(ret.cast<std::string>() == (a+b+c+d));
-	}
+    {
+        struct IncNumber {
+            unsigned n;
+            IncNumber() : n(1) {}
+            std::string operator()() 
+            { return boost::lexical_cast<std::string>(n++); }
+        };
 
-	{
-		const std::string prop("message");
-		CHECK(test->HasProperty(prop));
-		const std::string value("hi there");
-		test->SetProperty(prop, value);
-		FB::variant ret = test->GetProperty(prop);
-		CHECK(ret.cast<std::string>() == value);
-	}
+        const std::string method("concatMany");
+        CHECK(test->HasMethod("concatMany"));
 
-	{
-		const std::string prop("readOnlyMessage");
-		CHECK(test->HasProperty(prop));
-		const FB::variant ret1 = test->GetProperty(prop);
-		test->SetProperty(prop, ret1.convert_cast<std::string>()+"foo");
-		const FB::variant ret2 = test->GetProperty(prop);
-		CHECK(ret1.cast<std::string>() == ret2.convert_cast<std::string>());
-	}
+        const unsigned max_args = 20;
+        std::vector<std::string> strings(max_args);
+        std::generate(strings.begin(), strings.end(), IncNumber());
+                
+        for(unsigned i=1; i<=max_args; ++i)
+        {
+            FB::VariantList args(strings.begin(), strings.begin()+i);
+            FB::variant ret = test->Invoke(method, args);
+            const std::string expected = std::accumulate(strings.begin(), strings.begin()+i, std::string(""));
+            CHECK(ret.cast<std::string>() == expected);
+        }
+    }
 
-	{
-		// Test variant parameter type
-		const std::string method("getType");
-		CHECK(test->HasMethod(method));
+    {
+        const std::string prop("message");
+        CHECK(test->HasProperty(prop));
+        const std::string value("hi there");
+        test->SetProperty(prop, value);
+        FB::variant ret = test->GetProperty(prop);
+        CHECK(ret.cast<std::string>() == value);
+    }
+
+    {
+        const std::string prop("readOnlyMessage");
+        CHECK(test->HasProperty(prop));
+        const FB::variant ret1 = test->GetProperty(prop);
+        test->SetProperty(prop, ret1.convert_cast<std::string>()+"foo");
+        const FB::variant ret2 = test->GetProperty(prop);
+        CHECK(ret1.cast<std::string>() == ret2.convert_cast<std::string>());
+    }
+
+    {
+        // Test variant parameter type
+        const std::string method("getType");
+        CHECK(test->HasMethod(method));
         
-		FB::variant ret = test->Invoke(method, FB::VariantList(list_of((long)12)));
-		CHECK(ret.cast<std::string>() == "long");
+        FB::variant ret = test->Invoke(method, FB::VariantList(list_of((long)12)));
+        CHECK(ret.cast<std::string>() == "long");
 
-		ret = test->Invoke(method, FB::VariantList(list_of((double)12.4)));
-		CHECK(ret.cast<std::string>() == "double");
+        ret = test->Invoke(method, FB::VariantList(list_of((double)12.4)));
+        CHECK(ret.cast<std::string>() == "double");
 
-		ret = test->Invoke(method, FB::VariantList(list_of((bool)true)));
-		CHECK(ret.cast<std::string>() == "bool");
+        ret = test->Invoke(method, FB::VariantList(list_of((bool)true)));
+        CHECK(ret.cast<std::string>() == "bool");
 
-		ret = test->Invoke(method, FB::VariantList(list_of((void *)0x12)));
-		CHECK(ret.cast<std::string>() == "void *");
-	}
+        ret = test->Invoke(method, FB::VariantList(list_of((void *)0x12)));
+        CHECK(ret.cast<std::string>() == "void *");
+    }
 }
