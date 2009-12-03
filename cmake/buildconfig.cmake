6f2db46a0fdd14127b345021d4fd7c86885d9d22
Richard Bateman <taxilian@gmail.com>
2009-12-02 22:42:33 -0700

    - Fixed errors in compiler definitions that caused DEP crash

--- cmake/buildconfig.cmake
+++ cmake/buildconfig.cmake
@@ -1,19 +1,19 @@
 
     
 if (WIN32)
-	set(CMAKE_C_FLAGS                            "/DWIN32 /W3 /wd4996 /nologo /D UNICODE /D _UNICODE")
-	set(CMAKE_CXX_FLAGS                          "/DWIN32 /W3 /wd4996 /nologo /EHsc /wd4290 /D UNICODE /D _UNICODE")
+	set(CMAKE_C_FLAGS                            "/DWIN32 /W3 /wd4996 /nologo /D UNICODE /D _UNICODE /D _WINDOWS")
+	set(CMAKE_CXX_FLAGS                          "/DWIN32 /W3 /wd4996 /nologo /EHsc /wd4290 /D UNICODE /D _UNICODE /D _WINDOWS")
 	set(CMAKE_C_FLAGS_RELEASE                    "/MT /O1 /DNDEBUG")
 	set(CMAKE_CXX_FLAGS_RELEASE                  "/MT /O1 /DNDEBUG")
-	set(CMAKE_C_FLAGS_DEBUG                      "/MTd /Od /DDEBUG /D_DEBUG /ZI /RTC1")
-	set(CMAKE_CXX_FLAGS_DEBUG                    "/MTd /Od /DDEBUG /D_DEBUG /ZI /RTC1")
+	set(CMAKE_C_FLAGS_DEBUG                      "/MTd /Od /DDEBUG /D_DEBUG /ZI /RTC1 /Gm")
+	set(CMAKE_CXX_FLAGS_DEBUG                    "/MTd /Od /DDEBUG /D_DEBUG /ZI /RTC1 /Gm")
 	set(CMAKE_EXE_LINKER_FLAGS_DEBUG
-	    "${CMAKE_EXE_LINKER_FLAGS_DEBUG}         /NODEFAULTLIB:libc,libcd,msvcrt,msvcrtd")
+	    "${CMAKE_EXE_LINKER_FLAGS_DEBUG}         ")
 	set(CMAKE_EXE_LINKER_FLAGS_RELEASE
-	    "${CMAKE_EXE_LINKER_FLAGS_RELEASE}       /NODEFAULTLIB:libc,libcd,msvcrt,msvcrtd")
+	    "${CMAKE_EXE_LINKER_FLAGS_RELEASE}       ")
 	set(CMAKE_SHARED_LINKER_FLAGS_DEBUG
-	    "${CMAKE_SHARED_LINKER_FLAGS_DEBUG}      /NODEFAULTLIB:libc,libcd,msvcrt,msvcrtd")
+	    "${CMAKE_SHARED_LINKER_FLAGS_DEBUG}      /SUBSYSTEM:WINDOWS /MACHINE:X86")
 	set(CMAKE_SHARED_LINKER_FLAGS_RELEASE
-	    "${CMAKE_SHARED_LINKER_FLAGS_RELEASE}    /NODEFAULTLIB:libc,libcd,msvcrt,msvcrtd")
+	    "${CMAKE_SHARED_LINKER_FLAGS_RELEASE}    /SUBSYSTEM:WINDOWS /MACHINE:X86 /OPT:REF /OPT:ICF")
 endif()
 
