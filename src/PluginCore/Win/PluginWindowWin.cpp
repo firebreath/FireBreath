43c644f20c25cc473664dad0b1f705473cd0a5c8
Richard Bateman <taxilian@gmail.com>
2009-12-07 19:10:48 -0700

    - Subclassing of windows fixed - Width and Height defaults added to the test htm page - Ready to add windows events (mouse events, etc)

--- src/PluginCore/Win/PluginWindowWin.cpp
+++ src/PluginCore/Win/PluginWindowWin.cpp
@@ -7,19 +7,25 @@
 
 using namespace FB;
 
-PluginWindowWin::PluginWindowWin(HWND hWnd) : m_hWnd(hWnd)
+PluginWindowWin::PluginWindowMap FB::PluginWindowWin::m_windowMap;
+
+PluginWindowWin::PluginWindowWin(HWND hWnd) : m_hWnd(hWnd), lpOldWinProc(NULL)
 {
     // subclass window so we can intercept window messages 
-    //lpOldWinProc = SubclassWindow(m_hWnd, (WNDPROC)PluginWindowWin::_WinProc);
+    lpOldWinProc = SubclassWindow(m_hWnd, (WNDPROC)PluginWindowWin::_WinProc);
 
     // associate window with this object so that we can route events properly
-    //SetWindowLong(m_hWnd, GWL_USERDATA, (LONG)this);
+    m_windowMap[static_cast<void*>(m_hWnd)] = this;
 }
 
 PluginWindowWin::~PluginWindowWin()
 {
     // Unsubclass the window so that everything is as it was before we got it
-	//SubclassWindow(m_hWnd, lpOldWinProc);
+	SubclassWindow(m_hWnd, lpOldWinProc);
+
+    PluginWindowMap::iterator it = m_windowMap.find(static_cast<void*>(m_hWnd));
+    if (it != m_windowMap.end()) 
+        m_windowMap.erase(it);
 }
 
 bool PluginWindowWin::WinProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam, LRESULT lRes)
@@ -31,12 +37,15 @@ bool PluginWindowWin::WinProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam
 
 LRESULT CALLBACK PluginWindowWin::_WinProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
-	PluginWindowWin *win = (PluginWindowWin *)(GetWindowLong(hWnd, GWL_USERDATA));
+    PluginWindowMap::iterator it = m_windowMap.find(static_cast<void*>(hWnd));
+    if (it == m_windowMap.end()) 
+        return 0;
+	PluginWindowWin *win = it->second;
 
 	LRESULT lResult(0);
 	// Try to handle the event through the plugin instace; if that doesn't work, handle it through the default winproc
 	if (win->WinProc(hWnd, uMsg, wParam, lParam, lResult))
 		return lResult;
 	else
-		return win->lpOldWinProc(hWnd, uMsg, wParam, lParam);
+		return DefWindowProc(hWnd, uMsg, wParam, lParam);
 }
--- src/PluginCore/Win/PluginWindowWin.h
+++ src/PluginCore/Win/PluginWindowWin.h
@@ -5,6 +5,8 @@
 #include "Win/win_common.h"
 #include "PluginWindow.h"
 
+#include <map>
+
 namespace FB {
 
     class PluginWindowWin : public PluginWindow
@@ -16,7 +18,10 @@ namespace FB {
                                                           LPARAM lParam);
         HWND getHWND() { return m_hWnd; }
 
+        typedef std::map<void*,PluginWindowWin*> PluginWindowMap;
     protected:
+        static PluginWindowMap m_windowMap;
+
         bool WinProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParamm, LRESULT lRes);
     	WNDPROC lpOldWinProc;
         HWND m_hWnd;
