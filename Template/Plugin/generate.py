9d88d2d92b5838f8e7019d4a193cbaa8a1335567
Richard Bateman <taxilian@gmail.com>
2009-12-12 17:38:57 -0700

    - Added basic JSAPI objects to the generate.py script in preperation for adding functionality to generate a default API object.

--- Template/Plugin/generate.py
+++ Template/Plugin/generate.py
@@ -16,7 +16,7 @@ class Base(object):
     def __init__(self, **kwargs):
         for k, v in kwargs.items():
             if hasattr(self, k): setattr(self, k, v)
-        
+
         self.keys = AttrDictSimple(
             name     = ("Name", re.compile(r"^.+$"), "Name must be at least one character, and may not contain carriage returns."),
             ident    = ("Identifier", re.compile(r"^[a-zA-Z][a-zA-Z\d_]{2,}$"), "Identifier must be 3 or more alphanumeric characters (underscore allowed)."),
@@ -25,7 +25,7 @@ class Base(object):
             domain   = ("Domain", re.compile(r"^([a-zA-Z0-9]+(\-[a-zA-Z0-9]+)*\.)*[a-zA-Z0-9]+(\-[a-zA-Z0-9]+)*\.[a-zA-Z]{2,4}$"), "Domain must be a valid domain name."),
             mimetype = ("MIME type", re.compile(r"^[a-zA-Z]+\/[a-zA-Z\-]+$"), "Please use alphabetic characters and dashes in the format: application/x-firebreath"),
         )
-    
+
     def getValue(self, key, default):
         desc, regex, error = self.keys[key]
         value = raw_input("%s %s [%s]: " % (self.__class__.__name__, desc, default)) or default
@@ -34,6 +34,64 @@ class Base(object):
         else:
             print "Invalid syntax: %s" % error
             return self.getValue(key, default)
+
+class JSAPI(Base):
+    ident	 	= None
+    properties	= AttrDictSimple()
+    methods		= AttrDictSimple()
+
+class JSAPI_Member(Base):
+    ident		= None
+    type		= None
+    def __init__(self):
+        print "Initializing JSAPI_Member"
+        self.types = AttrDictSimple(
+            string	= "std::string",
+            int		= "int",
+            double	= "double",
+            bool	= "bool",
+            variant	= "FB::variant",
+            dynamic	= "FB::VariantList",
+            JSOBJ	= "FB::AutoPtr<FB::JSAPI>",
+            API		= "FB::AutoPtr<FB::BrowserObjectAPI>",
+        )
+
+    def translateType(self, type):
+        return self.types[type]
+
+    def isValidType(self, type):
+        return self.types.has_key(type)
+
+    def setType(self, type):
+        self.type = type
+
+    def getRealType(self):
+        return self.translateType(self.type)
+
+class JSAPI_Property(JSAPI_Member):
+    def __init__(self, ident, type):
+        super(JSAPI_Property, self).__init__()
+        if not self.isValidType(type):
+            raise Exception("Invalid type %s.  Valid types are: %s" % type, ', '.join(self.types))
+        self.type = type
+        self.ident = ident
+
+class JSAPI_Method(JSAPI_Member):
+    argTypes	= ["string"]
+    def __init__(self, ident, type, argTypes):
+        super(JSAPI_Method, self).__init__()
+        self.type = type
+        self.ident = ident
+        self.argTypes = argTypes
+        for curArg in argTypes:
+            if not self.isValidType(curArg):
+                raise Exception("Invalid type %s.  Valid types are: %s" % (curArg, ', '.join(self.types)))
+
+    def getRealArgTypes(self):
+        retVal = []
+        for cur in self.argTypes:
+            retVal.append(self.translateType(cur))
+        return retVal
 
 class Plugin(Base):
     name     = None
@@ -46,7 +104,7 @@ class Plugin(Base):
         MIN_LEN_PREFIX=3
         MAX_LEN_PREFIX=5
         pattern = re.compile(r"([A-Z][A-Z][a-z])|([a-z][A-Z])")
-        
+
         if startName is None:
             return None
         if MIN_LEN_PREFIX <= len(startName) <= MAX_LEN_PREFIX:
@@ -56,7 +114,7 @@ class Plugin(Base):
 
         seperated = normalize(pattern.sub(lambda m: m.group()[:1] + delim + m.group()[1:], startName))
 
-        words = seperated.split();
+        words = seperated.split()
 
         if MIN_LEN_PREFIX <= len(words) <= MAX_LEN_PREFIX:
             return "".join( [ lett[0] for lett in words ]).upper()[:MAX_LEN_PREFIX]
@@ -71,17 +129,17 @@ class Plugin(Base):
                 needed += 1
                 word -= 1
         return "".join( [ lett[0] for lett in words[:word] ] ).upper() + postfix.upper()
-        
+
     def __init__(self, **kwargs):
         super(Plugin, self).__init__(**kwargs)
-        
+
         self.name     = self.name or self.getValue("name", "")
         self.ident    = self.ident or self.getValue("ident", re.sub(r"[^a-zA-Z\d\-_]", "", self.name))
         self.prefix   = self.prefix or self.getValue("prefix", self.makeDefaultPrefix(self.name))
         self.mimetype = self.mimetype or self.getValue("mimetype", "application/x-%s" % self.ident.lower())
         self.mimetype = self.mimetype.lower()
         self.desc     = self.desc or self.getValue("desc", "")
-    
+
     def __str__(self):
         return "\nPlugin Details:\n--------------\nName:        %(name)s\nIdentifier:  %(ident)s\nPrefix:      %(prefix)s\nMIME type:   %(mimetype)s\nDescription: %(desc)s" % self
 
@@ -89,14 +147,14 @@ class Company(Base):
     name   = None
     ident  = None
     domain = None
-    
+
     def __init__(self, **kwargs):
         super(Company, self).__init__(**kwargs)
-        
+
         self.name   = self.name or self.getValue("name", "")
         self.ident  = self.ident or self.getValue("ident", re.sub(r"[^a-zA-Z\d\-_]", "", self.name))
         self.domain = self.domain or self.getValue("domain", "%s.com" % self.ident.lower())
-    
+
     def __str__(self):
         return "\nCompany Details\n---------------\nName:        %(name)s\nIdentifier:  %(ident)s\nDomain:      %(domain)s" % self
 
@@ -107,23 +165,23 @@ class GUID(Base):
             self.master = uuid.uuid4()
         else:
             self.master = uuid.uuid1()
-    
+
     def generate(self, string):
         return uuid.uuid5(self.master, string)
 
 # API
 #     id
-# 
+#
 # PROPERTY
 #     id
-# 
+#
 # METHOD
 #     id
 #     type (int, bool, double, std::string, FB::variant or FB::AutoPtr<JSAPI>)
-#     
+#
 # UUID
 #     generate(string)
-# 
+#
 
 def Main():
     # Make sure Cheetah is available
@@ -132,7 +190,7 @@ def Main():
     except:
         print "This utility requires a working install of the Cheetah template system (http://www.cheetahtemplate.org)"
         sys.exit(1)
-    
+
     # Define the command-line interface via OptionParser
     usage = "usage: %prog [options]"
     parser = OptionParser(usage)
@@ -144,12 +202,12 @@ def Main():
     parser.add_option("-4", "--uuid-version-4", dest = "uuidVersion4", action = "store_true", default = False,
         help = "Specifying this option will generate a Version 4 UUID, which avoids using your MAC address and current time but does not guarantee true uniqueness.")
     options, args = parser.parse_args()
-    
+
     # Instantiate the appropriate classes
     plugin = Plugin(name = options.pluginName, id = options.pluginIdent)
     company = Company(name = options.companyName)
     guid = GUID(useVersion4 = options.uuidVersion4)
-    
+
     # Make sure we can get into the projects directory
     basePath = os.path.abspath(os.path.join(__file__, "../../../projects"))
     if not os.path.isdir(basePath):
@@ -158,7 +216,7 @@ def Main():
         except:
             print "Unable to create directory", basePath
             sys.exit(1)
-    
+
     # Try to create a directory for this project
     projPath = os.path.abspath(os.path.join(basePath, "%s" % plugin.ident))
     if os.path.isdir(projPath):
@@ -172,7 +230,7 @@ def Main():
         except:
             print "Failed to create project directory", projPath
             sys.exit(1)
-    
+
     print "\nProcessing templates"
     templateFiles = ["CMakeLists.txt", "PluginConfig.cmake", "Win/projectDef.cmake"]
     for tpl in templateFiles:
@@ -180,7 +238,7 @@ def Main():
             filename = os.path.join(projPath, tpl)
             if not os.path.isdir(os.path.dirname(filename)):
                 os.mkdir(os.path.dirname(filename))
-            
+
             template = Template(file = tpl, searchList = [{"PLUGIN" : plugin, "COMPANY" : company, "GUID" : guid}])
             f = open(filename, "wb")
             f.write("%s" % template)
@@ -189,6 +247,6 @@ def Main():
             print "  Error processing", tpl
             raise
     print "Done. Files placed in", projPath
-    
+
 if __name__ == "__main__":
     Main()
