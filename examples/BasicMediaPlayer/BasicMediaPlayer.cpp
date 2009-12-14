0218bf527903c6fd4435ccb9e3103008f590fbd8
georg.fritzsche@gmail.com <georg.fritzsche@gmail.com>
2009-12-14 12:47:31 +0100

    - updated example to use JSArray

--- examples/BasicMediaPlayer/BasicMediaPlayer.cpp
+++ examples/BasicMediaPlayer/BasicMediaPlayer.cpp
@@ -17,8 +17,8 @@ Copyright 2009 Georg Fritzsche,
 #include "BasicMediaPlayer.h"
 #include "MediaPlayer.h"
 #include "DOM/JSAPI_DOMWindow.h"
-
-#include <windows.h>
+#include "Util/JSArray.h"
+#include "variant_list.h"
 
 BasicMediaPlayer::BasicMediaPlayer(FB::BrowserHostWrapper *host) 
   : m_host(host)
@@ -26,90 +26,67 @@ BasicMediaPlayer::BasicMediaPlayer(FB::BrowserHostWrapper *host)
   , m_valid(false)
   , m_version("")
 {
-	using FB::make_method;
-	using FB::make_property;
+    using FB::make_method;
+    using FB::make_property;
 
-	registerMethod  ("play",     make_method  (this, &BasicMediaPlayer::Play));
-	registerMethod  ("stop",     make_method  (this, &BasicMediaPlayer::Stop));
-	registerProperty("version",  make_property(this, &BasicMediaPlayer::Version));
-	registerProperty("type",     make_property(this, &BasicMediaPlayer::Type));
-	registerProperty("playlist", make_property(this, &BasicMediaPlayer::Playlist,
-													 &BasicMediaPlayer::SetPlaylist));
+    registerMethod  ("play",     make_method  (this, &BasicMediaPlayer::Play));
+    registerMethod  ("stop",     make_method  (this, &BasicMediaPlayer::Stop));
+    registerProperty("version",  make_property(this, &BasicMediaPlayer::Version));
+    registerProperty("type",     make_property(this, &BasicMediaPlayer::Type));
+    registerProperty("playlist", make_property(this, &BasicMediaPlayer::Playlist,
+                                                     &BasicMediaPlayer::SetPlaylist));
 
-	try 
-	{
-		m_player  = MediaPlayerPtr(new MediaPlayer);
-		m_valid   = true;
-	} 
-	catch(MediaPlayer::InitializationException&) 
-	{
-		m_player.reset();
-	}
+    try 
+    {
+        m_player  = MediaPlayerPtr(new MediaPlayer);
+        m_valid   = true;
+    } 
+    catch(MediaPlayer::InitializationException&) 
+    {
+        m_player.reset();
+    }
 }
 
 BasicMediaPlayer::~BasicMediaPlayer()
 {
-	m_player.reset();
+    m_player.reset();
 }
 
 bool BasicMediaPlayer::Play()
 {
-	return false;
+    return false;
 }
 
 bool BasicMediaPlayer::Stop()
 {
-	return false;
+    return false;
 }
 
 std::string BasicMediaPlayer::Version()
 {
-	return m_player->Version();
+    return m_player->Version();
 }
 
 std::string BasicMediaPlayer::Type()
 {
-	return m_player->Type();
+    return m_player->Type();
 }
 
 FB::JSAPI* BasicMediaPlayer::Playlist()
 {
-	return 0;
+    return 0;
 }
 
 void BasicMediaPlayer::SetPlaylist(FB::AutoPtr<FB::BrowserObjectAPI> obj)
 {
-	if(!obj->HasProperty("length"))
-		throw FB::invalid_arguments("expected array parameter");
-	
-	FB::variant res = obj->GetProperty("length");
-	long length = 0;
-	try {
-		length = res.convert_cast<long>();
-	} catch(FB::bad_variant_cast&) {
-		throw FB::script_error("length returned unexpected type");
-	}
+    typedef std::vector<std::string> StringVec;
+    StringVec entries = FB::convert_variant_list<StringVec>(FB::JSArray(obj).Values());
 
-	std::vector<std::string> entries;
-
-	try 
-	{
-		for(long i=0; i<length; ++i) 
-		{
-			res = obj->GetProperty(i);
-			entries.push_back(res.convert_cast<std::string>());
-		}
-	}
-	catch(FB::bad_variant_cast&)
-	{
-		throw FB::script_error("could not convert entry in playlist to string");
-	}
-
-	std::string s;
-	for(std::vector<std::string>::iterator it = entries.begin(); it != entries.end(); ++it)
-	{
-		s += *it + "\n";
-	}
-	
-	m_host->getDOMWindow().alert(s);
+    std::string s;
+    for(std::vector<std::string>::iterator it = entries.begin(); it != entries.end(); ++it)
+    {
+        s += "'" + *it + "'\n";
+    }
+    
+    m_host->getDOMWindow().alert(s);
 }
