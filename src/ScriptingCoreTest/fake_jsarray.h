ade7180e2ebd5d4457fd73d09971c7b1677696b6
georg.fritzsche@gmail.com <georg.fritzsche@gmail.com>
2009-12-16 07:39:08 +0100

    - added license

--- src/ScriptingCoreTest/fake_jsarray.h
+++ src/ScriptingCoreTest/fake_jsarray.h
@@ -1,3 +1,16 @@
+/**********************************************************\ 
+Original Author: Georg Fritzsche
+
+Created:    Dec 16, 2009
+License:    Dual license model; choose one of two:
+            Eclipse Public License - Version 1.0
+            http://www.eclipse.org/legal/epl-v10.html
+            - or -
+            GNU Lesser General Public License, version 2.1
+            http://www.gnu.org/licenses/lgpl-2.1.html
+
+Copyright 2009 Georg Fritzsche, Firebreath development team
+\**********************************************************/
 
 #include "BrowserObjectAPI.h"
 
