1c4f8397e2b020c5e2689465e019141c9b64d43b
georg.fritzsche@gmail.com <georg.fritzsche@gmail.com>
2009-12-22 23:39:39 +0100

    - added unit test for variant_map_of()

---
+++ src/ScriptingCoreTest/fake_jsmap.h
@@ -0,0 +1,91 @@
+/**********************************************************\ 
+Original Author: Georg Fritzsche
+
+Created:    Dec 22, 2009
+License:    Dual license model; choose one of two:
+            Eclipse Public License - Version 1.0
+            http://www.eclipse.org/legal/epl-v10.html
+            - or -
+            GNU Lesser General Public License, version 2.1
+            http://www.gnu.org/licenses/lgpl-2.1.html
+
+Copyright 2009 Georg Fritzsche, Firebreath development team
+\**********************************************************/
+
+#ifndef H_FAKE_JS_MAP
+#define H_FAKE_JS_MAP
+
+#include "BrowserObjectAPI.h"
+
+class FakeJsMap : public FB::BrowserObjectAPI
+{
+    typedef std::vector<std::string> StringVec;
+    typedef FB::JSOutMap::value_type OutPair;
+
+    struct GrabKeys {
+        std::back_insert_iterator<StringVec> inserter;
+        GrabKeys(StringVec& keys) : inserter(std::back_inserter(keys)) {}
+        void operator()(const OutPair& p) {
+            *inserter++ = p.first;
+        }
+    };
+public:
+    FakeJsMap(const FB::JSOutMap& values)
+      : m_values(values), FB::BrowserObjectAPI(0)
+    {
+        std::for_each(m_values.begin(), m_values.end(), GrabKeys(m_names));
+    }
+
+    bool HasMethod(std::string) { return false; }
+    void FB::JSAPI::SetProperty(int,const FB::variant) {}
+    void FB::JSAPI::SetProperty(std::string,const FB::variant) {}
+    FB::variant FB::JSAPI::Invoke(std::string,FB::VariantList&) { return FB::variant(); }
+
+    // Methods for enumeration
+    virtual void getMemberNames(StringVec &names) 
+    {
+        names = m_names;
+    }
+    
+    virtual size_t getMemberCount() 
+    {
+        return m_values.size(); 
+    }
+
+
+    bool HasProperty(std::string s)    
+    { 
+        return (s == "length"); 
+    }
+    
+    bool HasProperty(int index)        
+    { 
+        return ((unsigned)index < m_values.size()); 
+    }
+    
+    FB::variant GetProperty(std::string s) 
+    { 
+        if(s == "length")
+            return (int)m_values.size();
+        FB::JSOutMap::const_iterator it = m_values.find(s);
+        if(it != m_values.end())
+            return it->second;
+        throw FB::script_error(std::string("no such property '")+s+"'");
+    }
+    
+    FB::variant GetProperty(int index)     
+    { 
+        if((unsigned)index >= m_values.size()) {
+            std::ostringstream ss;
+            ss << "index out of range - got " << index << ", size is " << m_values.size();
+            throw FB::script_error(ss.str());
+        }
+        return m_values[m_names[index]];
+    }
+
+private:
+    FB::JSOutMap m_values;
+    StringVec m_names;
+};
+
+#endif
---
+++ src/ScriptingCoreTest/variant_map_test.h
@@ -0,0 +1,44 @@
+/**********************************************************\ 
+Original Author: Georg Fritzsche
+
+Created:    Dec 13, 2009
+License:    Dual license model; choose one of two:
+            Eclipse Public License - Version 1.0
+            http://www.eclipse.org/legal/epl-v10.html
+            - or -
+            GNU Lesser General Public License, version 2.1
+            http://www.gnu.org/licenses/lgpl-2.1.html
+
+Copyright 2009 Georg Fritzsche, Firebreath development team
+\**********************************************************/
+
+
+#include <map>
+#include <boost/assign/list_of.hpp>
+#include "APITypes.h"
+#include "variant_map.h"
+
+TEST(VariantMapTest)
+{
+    PRINT_TESTNAME;
+
+    using boost::assign::list_of;
+    
+    {
+        typedef std::map<std::string, FB::variant> VariantMap;
+        typedef std::pair<VariantMap::key_type, VariantMap::mapped_type> MapPair;
+        typedef std::vector<std::string> StringVec;
+        
+        StringVec names = list_of("a")("b")("c")("d")("e");
+        VariantMap values = FB::variant_map_of<std::string>("a","a")("b","b")("c","c")("d","d")("e","e");
+
+        StringVec::const_iterator  itn = names.begin();
+        VariantMap::const_iterator itv = values.begin();
+
+        for( ; itn!=names.end() && itv!=values.end(); ++itn, ++itv)
+        {
+            CHECK(*itn == itv->first);
+            CHECK(*itn == itv->second.convert_cast<std::string>());
+        }
+    }
+}
