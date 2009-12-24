f2434437b8a426cdc16f1f83a163dc8046f8abe3
georg.fritzsche@gmail.com <georg.fritzsche@gmail.com>
2009-12-24 02:33:48 +0100

    - example plugin: extended error mapping

--- examples/BasicMediaPlayer/Win/MediaPlayerWin.cpp
+++ examples/BasicMediaPlayer/Win/MediaPlayerWin.cpp
@@ -5,6 +5,7 @@
 #include <dshow.h>
 #include "JSAPI.h"
 #include "../MediaPlayer.h"
+#include "error_mapping.h"
 
 #pragma warning(disable:4995)
 
@@ -22,6 +23,9 @@ MediaPlayer::MediaPlayer()
   , m_version("")
   , m_type("DirectShow")
 {
+    ::CoInitialize(0);
+
+#if 0
     m_context = PlayerContextPtr(new PlayerContext);
     if(!m_context) throw InitializationException("failed to create context");
 
@@ -36,6 +40,7 @@ MediaPlayer::MediaPlayer()
 
     m_context->spMediaControl = spMediaControl;
     m_context->spMediaEvent   = spMediaEvent;
+#endif
 }
 
 MediaPlayer::~MediaPlayer()
@@ -53,48 +58,36 @@ const std::string& MediaPlayer::Type() const
     return m_type;
 }
 
-std::string vfwError(HRESULT hr)
-{
-#define ENTRY(X) case X: return #X;
-    switch(hr)
-    {
-        default: return "<unknown>";
-        ENTRY(S_OK);
-        ENTRY(VFW_S_AUDIO_NOT_RENDERED);
-        ENTRY(VFW_S_DUPLICATE_NAME);
-        ENTRY(VFW_S_PARTIAL_RENDER);    
-        ENTRY(VFW_S_VIDEO_NOT_RENDERED);
-        ENTRY(E_ABORT);    
-        ENTRY(E_FAIL);    
-        ENTRY(E_INVALIDARG);    
-        ENTRY(E_OUTOFMEMORY);    
-        ENTRY(E_POINTER);    
-        ENTRY(VFW_E_CANNOT_CONNECT);    
-        ENTRY(VFW_E_CANNOT_LOAD_SOURCE_FILTER);    
-        ENTRY(VFW_E_CANNOT_RENDER);    
-        ENTRY(VFW_E_INVALID_FILE_FORMAT);    
-        ENTRY(VFW_E_NOT_FOUND);    
-        ENTRY(VFW_E_UNKNOWN_FILE_TYPE);    
-        ENTRY(VFW_E_UNSUPPORTED_STREAM);
-    }
-#undef ENTRY
-}
-
 std::string MediaPlayer::play(const std::string& file)
 {
     HRESULT hr;
 
+    m_context = PlayerContextPtr(new PlayerContext);
+    if(!m_context) throw InitializationException("failed to create context");
+
+    hr =  m_context->spGraph.CoCreateInstance(CLSID_FilterGraph, 0, CLSCTX_INPROC_SERVER);
+    if(FAILED(hr)) throw InitializationException("failed to create player");
+    
+    CComQIPtr<IMediaControl> spMediaControl(m_context->spGraph);
+    if(!spMediaControl) throw InitializationException("failed to QI for IMediaControl");
+
+    CComQIPtr<IMediaEvent> spMediaEvent(m_context->spGraph);
+    if(!spMediaEvent) throw InitializationException("failed to QI for IMediaEvent");
+
+    m_context->spMediaControl = spMediaControl;
+    m_context->spMediaEvent   = spMediaEvent;
+
     hr = m_context->spGraph->RenderFile(L"c:\\tmp\\weare.wav", 0);
     if(FAILED(hr)) {
         std::ostringstream ss;
-        ss << "failed to render file: " << vfwError(hr);
+        ss << "failed to render file: " << mapVfwError(hr);
         return ss.str();
     }
 
     hr = m_context->spMediaControl->Run();
     if(FAILED(hr)) {
         std::ostringstream ss;
-        ss << "failed to render file: " << vfwError(hr);
+        ss << "failed to render file: " << mapVfwError(hr);
         return ss.str();
     }
 
---
+++ examples/BasicMediaPlayer/Win/error_mapping.h
@@ -0,0 +1,9 @@
+
+#ifndef H_ERROR_MAPPING_23122009
+#define H_ERROR_MAPPING_23122009
+
+#include <windows.h>
+
+std::string mapVfwError(HRESULT hr);
+
+#endif
