5e87874d5f476c1ae9f51a0f1e6f2fd93cd04b09
georg.fritzsche@gmail.com <georg.fritzsche@gmail.com>
2010-02-03 02:45:49 +0100

    removed some undesired license blocks

--- fbgen/src/TemplateAPI.cpp
+++ fbgen/src/TemplateAPI.cpp
@@ -1,16 +1,7 @@
 /**********************************************************\
-Original Author: Richard Bateman and Georg Fritzsche
 
-Created:    December 3, 2009
-License:    Dual license model; choose one of two:
-            Eclipse Public License - Version 1.0
-            http://www.eclipse.org/legal/epl-v10.html
-            - or -
-            GNU Lesser General Public License, version 2.1
-            http://www.gnu.org/licenses/lgpl-2.1.html
+  Auto-generated @{PLUGIN.ident}API.cpp
 
-Copyright 2009 PacketPass Inc, Georg Fritzsche,
-               Firebreath development team
 \**********************************************************/
 
 #include "BrowserObjectAPI.h"
