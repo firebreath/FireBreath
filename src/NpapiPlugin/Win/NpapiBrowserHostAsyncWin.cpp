fa8f77cf0695d669566b02c454aafb0de7fa8314
georg.fritzsche@gmail.com <georg.fritzsche@gmail.com>
2010-03-01 05:47:32 +0100

    added missing license.

--- src/NpapiPlugin/Win/NpapiBrowserHostAsyncWin.cpp
+++ src/NpapiPlugin/Win/NpapiBrowserHostAsyncWin.cpp
@@ -1,4 +1,18 @@
 
+/**********************************************************\
+Original Author: Georg Fritzsche
+
+Created:    March 1, 2010
+License:    Dual license model; choose one of two:
+            Eclipse Public License - Version 1.0
+            http://www.eclipse.org/legal/epl-v10.html
+            - or -
+            GNU Lesser General Public License, version 2.1
+            http://www.gnu.org/licenses/lgpl-2.1.html
+
+Copyright 2009 Georg Fritzsche, Firebreath development team
+\**********************************************************/
+
 #include "NpapiBrowserHostAsyncWin.h"
 #include "Win/PluginWindowWin.h"
 
