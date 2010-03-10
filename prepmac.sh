fa6eefd73a0a39f0445280fb90a76f4242061197
Richard Bateman <taxilian@gmail.com>
2010-03-10 14:05:32 -0700

    Fixed issue where xcode patch only worked on examples

--- prepmac.sh
+++ prepmac.sh
@@ -5,14 +5,16 @@ GEN='Xcode'
 if [ "$1" = "examples" ]; then
     echo Building example projects
     cmake -D GEN="${GEN}" -D BUILD_EXAMPLES="YES" -P cmake/genproject.cmake
+    BUILDDIR=buildex
 else
     echo Building projects
     cmake -D GEN="${GEN}" -P cmake/genproject.cmake
+    BUILDDIR=build
 fi
 
 if [ -f "cmake/patch_xcode.py" ]; then
     while read target proj
     do
         python cmake/patch_xcode.py -f "$proj" -t "$target"
-    done < buildex/xcode_patch_desc.txt
+    done < $BUILDDIR/xcode_patch_desc.txt
 fi
