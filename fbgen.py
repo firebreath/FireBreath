b1f90a4598453639a6c4bc06a3e7001e4f77a701
Richard Bateman <taxilian@gmail.com>
2010-05-26 10:01:47 -0600

    Finished license change

--- fbgen.py
+++ fbgen.py
@@ -6,8 +6,8 @@ Utility script to generate/modify Firebreath plug-in projects.
 Original Author(s): Ben Loveridge, Richard Bateman
 Created:    14 December 2009
 License:    Dual license model; choose one of two:
-            Eclipse Public License - Version 1.0
-            http://www.eclipse.org/legal/epl-v10.html
+            New BSD License
+            http://www.opensource.org/licenses/bsd-license.php
             - or -
             GNU Lesser General Public License, version 2.1
             http://www.gnu.org/licenses/lgpl-2.1.html
