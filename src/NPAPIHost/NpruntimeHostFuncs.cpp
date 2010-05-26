8b305df61dbe678e6f1cd4350971d27cf22c5715
Richard Bateman <taxilian@gmail.com>
2010-05-26 09:56:32 -0600

    Changed license on most files to new BSD

--- src/NPAPIHost/NpruntimeHostFuncs.cpp
+++ src/NPAPIHost/NpruntimeHostFuncs.cpp
@@ -3,8 +3,8 @@ Original Author: Richard Bateman (taxilian)
 
 Created:    Oct 15, 2009
 License:    Dual license model; choose one of two:
-            Eclipse Public License - Version 1.0
-            http://www.eclipse.org/legal/epl-v10.html
+            New BSD License
+            http://www.opensource.org/licenses/bsd-license.php
             - or -
             GNU Lesser General Public License, version 2.1
             http://www.gnu.org/licenses/lgpl-2.1.html
--- src/ScriptingCoreTest/fake_jsmap.h
+++ src/ScriptingCoreTest/fake_jsmap.h
@@ -3,8 +3,8 @@ Original Author: Georg Fritzsche
 
 Created:    Dec 22, 2009
 License:    Dual license model; choose one of two:
-            Eclipse Public License - Version 1.0
-            http://www.eclipse.org/legal/epl-v10.html
+            New BSD License
+            http://www.opensource.org/licenses/bsd-license.php
             - or -
             GNU Lesser General Public License, version 2.1
             http://www.gnu.org/licenses/lgpl-2.1.html
