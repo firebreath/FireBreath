c489ad17008e6604e4f4352de75cfd7af34e52eb
Richard Bateman <taxilian@gmail.com>
2010-09-07 18:22:02 -0600

    Initial import of Dan Weatherford's utf8 conversion libs

---
+++ src/ScriptingCore/utf8_tools.cpp
@@ -0,0 +1,87 @@
+/**********************************************************\
+Original Author: Dan Weatherford
+Imported with permission by: Richard Bateman (taxilian)
+
+Created:    Sept 24, 2009
+License:    Dual license model; choose one of two:
+            New BSD License
+            http://www.opensource.org/licenses/bsd-license.php
+            - or -
+            GNU Lesser General Public License, version 2.1
+            http://www.gnu.org/licenses/lgpl-2.1.html
+
+Copyright 2009 Dan Weatherford, Facebook inc
+\**********************************************************/
+
+#include "utf8_tools.h"
+#include <stdlib.h>
+#include <limits.h>
+#include <boost/scoped_array.hpp>
+#include <stdexcept>
+
+#ifdef _WIN32
+#include "windows_defs.h"
+#include <windows.h>
+#else
+#include <xlocale.h>
+#include <wctype.h>
+#endif
+
+namespace FB {
+
+    std::string wstring_to_utf8(const std::wstring& src) {
+#ifdef _WIN32
+      int buffer_len = WideCharToMultiByte(CP_UTF8, 0, src.data(), src.size()+1, NULL, 0, NULL, NULL);
+      if (buffer_len <= 0) throw_GetLastError("WideCharToMultiByte");
+      std::string out_str;
+      out_str.resize(buffer_len - 1);
+      WideCharToMultiByte(CP_UTF8, 0, src.data(), src.size()+1, const_cast<char*>(out_str.data()), (int)(out_str.size() + 1), NULL, NULL);
+      return out_str;
+#else
+      const size_t work_size = (src.size() * MB_LEN_MAX) + 1;
+      boost::scoped_array<char> work(new char[work_size]);
+      size_t res = wcstombs(work.get(), src.data(), work_size);
+      if (res == ((size_t)-1)) throw std::runtime_error("wstring_to_utf8: conversion error");
+      return std::string(work.get());
+#endif
+    }
+
+
+    std::wstring utf8_to_wstring(const std::string& src) {
+#ifdef _WIN32
+      if (src.empty()) return std::wstring();
+      int buffer_len = MultiByteToWideChar(CP_UTF8, 0, src.data(), src.size()+1, NULL, 0);
+      if (buffer_len <= 0) throw_GetLastError("MultiByteToWideChar");
+      std::wstring res;
+      res.resize(buffer_len - 1);
+      MultiByteToWideChar(CP_UTF8, 0, src.data(), src.size()+1, const_cast<wchar_t*>(res.data()), (int)(res.size()+1));
+      return res;
+#else
+      const size_t work_size = src.size() + 1;
+      boost::scoped_array<wchar_t> work(new wchar_t[work_size]);
+      size_t res = mbstowcs(work.get(), src.data(), work_size);
+      if (res == ((size_t)-1)) throw std::runtime_error("utf8_to_wstring: conversion error");
+      return std::wstring(work.get());
+#endif
+    }
+
+
+    std::wstring wstring_tolower(const std::wstring& src) {
+      if (src.empty()) return std::wstring();
+      
+      std::wstring dest;
+      dest.resize(src.size());
+      
+#ifdef _WIN32
+      if (LCMapString(LOCALE_USER_DEFAULT, LCMAP_LOWERCASE, src.data(), src.size(), const_cast<wchar_t*>(dest.data()), dest.size()) == 0) throw_GetLastError("wstring_tolower");
+      
+#else
+      for (size_t i = 0; i < src.size(); ++i) {
+        dest[i] = towlower(src[i]);
+      }
+#endif
+
+      return dest;
+    }
+
+};
