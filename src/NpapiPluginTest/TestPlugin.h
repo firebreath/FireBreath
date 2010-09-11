fa38a95cfce2314d1c7d855a3c3315e60fd12d07
Richard Bateman <taxilian@gmail.com>
2010-09-11 11:16:50 -0600

    Added missing file for npapi unit tests (which don't yet work)

---
+++ src/NpapiPluginTest/TestPlugin.h
@@ -0,0 +1,40 @@
+/**********************************************************\ 
+Original Author: Richard Bateman (taxilian)
+
+Created:    Sep 10, 2010
+License:    Dual license model; choose one of two:
+            New BSD License
+            http://www.opensource.org/licenses/bsd-license.php
+            - or -
+            GNU Lesser General Public License, version 2.1
+            http://www.gnu.org/licenses/lgpl-2.1.html
+
+Copyright 2009 Richard Bateman, Firebreath development team
+\**********************************************************/
+#ifndef H_TESTPLUGIN
+#define H_TESTPLUGIN
+
+#include "PluginCore.h"
+#include "TestJSAPI.h"
+
+class TestPlugin : public FB::PluginCore
+{
+public:
+    static int init;
+    static void StaticInitialize() { init = 1; }
+    static void StaticDeinitialize() { init = 0; }
+
+public:
+    TestPlugin() { };
+    virtual ~TestPlugin() {};
+
+public:
+    virtual FB::JSAPIPtr createJSAPI() { return FB::JSAPIPtr(new TestObjectJSAPI()); }
+    virtual bool IsWindowless() { return false; }
+    bool HandleEvent(FB::PluginEvent *,FB::PluginEventSource *) { return false; }
+
+};
+
+int TestPlugin::init = 0;
+
+#endif
