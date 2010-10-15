5f9efa912f141dcc222b3c3f18308ffb594f267d
Richard Bateman <taxilian@gmail.com>
2010-10-15 13:39:52 -0600

    Added minimal docs to NPObjectAPI and IDispatchAPI

--- src/NpapiPlugin/NPObjectAPI.h
+++ src/NpapiPlugin/NPObjectAPI.h
@@ -24,6 +24,11 @@ Copyright 2009 Richard Bateman, Firebreath development team
 
 namespace FB { namespace Npapi {
 
+    ////////////////////////////////////////////////////////////////////////////////////////////////////
+    /// @class  NPObjectAPI
+    ///
+    /// @brief  Provides a FB::JSObject implementation that wraps a NPObject*
+    ////////////////////////////////////////////////////////////////////////////////////////////////////
     class NPObjectAPI :
         public FB::JSObject
     {
@@ -35,7 +40,6 @@ namespace FB { namespace Npapi {
         void *getEventContext() { return browser->getContextID(); };
         NPObject *getNPObject() { return obj; }
 
-        // Enumerate members
         void getMemberNames(std::vector<std::string> &nameVector);
         size_t getMemberCount();
     protected:
@@ -43,19 +47,16 @@ namespace FB { namespace Npapi {
         NPObject *obj;
 
     public:
-        // Methods to query existance of members on the API
         bool HasMethod(const std::string& methodName);
         bool HasProperty(const std::string& propertyName);
         bool HasProperty(int idx);
         bool HasEvent(const std::string& eventName);
 
-        // Methods to manage properties on the API
         variant GetProperty(const std::string& propertyName);
         void SetProperty(const std::string& propertyName, const variant& value);
         variant GetProperty(int idx);
         void SetProperty(int idx, const variant& value);
 
-        // Methods to manage methods on the API
         variant Invoke(const std::string& methodName, const std::vector<variant>& args);
     };
 
