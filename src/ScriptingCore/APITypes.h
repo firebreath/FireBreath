46807f131fea2442b56a4370b7c4a003e84bef03
Richard Bateman <taxilian@gmail.com>
2010-10-15 01:04:28 -0600

    Added docs to APITypes.h

--- src/ScriptingCore/APITypes.h
+++ src/ScriptingCore/APITypes.h
@@ -26,6 +26,18 @@ Copyright 2009 Richard Bateman, Firebreath development team
 // get rid of "unused variable" warnings
 #define FB_UNUSED_VARIABLE(x) ((void)(x))
 
+////////////////////////////////////////////////////////////////////////////////////////////////////
+/// @namespace  FB
+///
+/// @brief  Primary location of FireBreath classes and utility functions.
+/// 		
+/// The fuve most important classes to understand when implementing a FireBreath plugin are:
+///   - FB::PluginCore
+///   - FB::JSAPI / FB::JSAPIAuto
+///   - FB::BrowserHost
+///   - FB::JSObject
+///   - FB::variant
+////////////////////////////////////////////////////////////////////////////////////////////////////
 namespace FB
 {
     class BrowserHost;
@@ -35,20 +47,76 @@ namespace FB
     
     // Variant list
 
+    ////////////////////////////////////////////////////////////////////////////////////////////////////
+    /// @typedef    std::vector<variant> VariantList
+    ///
+    /// @brief  Defines an alias representing list of variants.
+    ////////////////////////////////////////////////////////////////////////////////////////////////////
     typedef std::vector<variant> VariantList;
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////
+    /// @typedef    std::map<std::string, variant> VariantMap
+    ///
+    /// @brief  Defines an alias representing a string -> variant map.
+    ////////////////////////////////////////////////////////////////////////////////////////////////////
     typedef std::map<std::string, variant> VariantMap;
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////
+    /// @typedef    std::set<std::string> StringSet
+    ///
+    /// @brief  Defines an alias representing a set of std::strings
+    ////////////////////////////////////////////////////////////////////////////////////////////////////
     typedef std::set<std::string> StringSet;
     
     // FB pointer types
-    
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////
+    /// @typedef    boost::shared_ptr<FB::JSAPI> JSAPIPtr
+    ///
+    /// @brief  Defines an alias for a JSAPI shared_ptr (you should never use a JSAPI* directly)
+    ////////////////////////////////////////////////////////////////////////////////////////////////////
     typedef boost::shared_ptr<FB::JSAPI> JSAPIPtr; 
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////
+    /// @typedef    boost::shared_ptr<FB::JSObject> JSObjectPtr
+    ///
+    /// @brief  Defines an alias representing a JSObject shared_ptr (you should never use a 
+    /// 		JSObject* directly)
+    ////////////////////////////////////////////////////////////////////////////////////////////////////
     typedef boost::shared_ptr<FB::JSObject> JSObjectPtr;
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////
+    /// @typedef    boost::shared_ptr<FB::BrowserHost> BrowserHostPtr
+    ///
+    /// @brief  Defines an alias representing a BrowserHost shared_ptr (you should never use a 
+    /// 		BrowserHost* directly)
+    ////////////////////////////////////////////////////////////////////////////////////////////////////
     typedef boost::shared_ptr<FB::BrowserHost> BrowserHostPtr;
     
     // backwards compability typedefs
-    
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////
+    /// @typedef    BrowserHost BrowserHostWrapper
+    ///
+    /// @brief  Defines a alias for backwards compatibility
+    /// @deprecated
+    ////////////////////////////////////////////////////////////////////////////////////////////////////
     typedef BrowserHost BrowserHostWrapper;
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////
+    /// @typedef    JSObject BrowserObjectAPI
+    ///
+    /// @brief  Defines a alias for backwards compatibility
+    /// @deprecated
+    ////////////////////////////////////////////////////////////////////////////////////////////////////
     typedef JSObject BrowserObjectAPI;
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////
+    /// @typedef    JSAPIPtr JSOutObject
+    ///
+    /// @brief  Defines an alias for JSOutObject -> JSAPIPtr
+    /// @deprecated
+    ////////////////////////////////////////////////////////////////////////////////////////////////////
     typedef JSAPIPtr JSOutObject;
     
     // deprecation warnings
@@ -62,7 +130,18 @@ namespace FB
 #endif
     
     // dynamically cast a FB pointer
-    
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////
+    /// @fn template<class T, class U> boost::shared_ptr<T> ptr_cast(boost::shared_ptr<U> const & r)
+    ///
+    /// @brief  Convenience function for doing a dynamic cast of one boost::shared_ptr to another
+    ///
+    /// This is simply an alias for boost::dynamic_ptr_cast<T>
+    /// 
+    /// @param  r   The value to cast
+    ///
+    /// @return A boost::shared_ptr<T>; if the dynamic cast failed this will be empty
+    ////////////////////////////////////////////////////////////////////////////////////////////////////
     template<class T, class U> 
     boost::shared_ptr<T> ptr_cast(boost::shared_ptr<U> const & r);
 
@@ -71,7 +150,15 @@ namespace FB
     typedef void (JSAPI::*SetPropertyType)(const std::string&, const variant&);
     typedef variant (JSAPI::*GetPropertyType)(const std::string&);
 
-    // helper type to allow JSAPIAuto catching of a list of variant arguments
+    ////////////////////////////////////////////////////////////////////////////////////////////////////
+    /// @struct CatchAll
+    ///
+    /// @brief  When used as a parameter on a JSAPIAuto function this matches 0 or more variants
+    /// 		-- in other words, all other parameters from this point on regardless of type.
+    ///
+    /// @author Richard Bateman
+    /// @date   10/15/2010
+    ////////////////////////////////////////////////////////////////////////////////////////////////////
     struct CatchAll {
         typedef FB::VariantList value_type;
         FB::VariantList value;
