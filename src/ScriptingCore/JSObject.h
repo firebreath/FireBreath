e4ee658ca2cd4e3db6f86e73774d55a211e070f9
Richard Bateman <taxilian@gmail.com>
2010-10-15 00:09:17 -0600

    added docs for JSObject

--- src/ScriptingCore/JSObject.h
+++ src/ScriptingCore/JSObject.h
@@ -22,26 +22,95 @@ Copyright 2009 Richard Bateman, Firebreath development team
 
 namespace FB
 {
+    ////////////////////////////////////////////////////////////////////////////////////////////////////
+    /// @class  JSObject
+    ///
+    /// @brief  Wraps a Javascript Object
+    /// 		
+    /// Whenever you access a javascript object on a page, it will be through this object.  Because
+    /// JSObject extends JSAPI, the API will be the same as a JSAPI object other than a few additional
+    /// methods provided on this object for convenience.
+    /// 
+    /// Implementations of JSObject are expected to be threadsafe as of 1.3.0.  What that usually
+    /// means is that if you call a method from another thread that it can't be safely run on it will
+    /// use BrowserHost::CallOnMainThread to make the call on the main thread and wait for it to
+    /// complete.  There may be performance issues with this that should be taken into consideration.
+    ///
+    /// @see JSAPI
+    /// @see NPObjectAPI
+    /// @see IDispatchAPI
+    ////////////////////////////////////////////////////////////////////////////////////////////////////
     class JSObject : public FB::JSAPI
     {
     public:
+
+        ////////////////////////////////////////////////////////////////////////////////////////////////////
+        /// @fn :::JSObject(BrowserHostPtr h)
+        ///
+        /// @brief  Constructor
+        ///
+        /// @param  h   The BrowserHost from whence the object came. 
+        ////////////////////////////////////////////////////////////////////////////////////////////////////
         JSObject(BrowserHostPtr h);
+
+        ////////////////////////////////////////////////////////////////////////////////////////////////////
+        /// @fn virtual :::~JSObject()
+        ///
+        /// @brief  Finaliser. 
+        ////////////////////////////////////////////////////////////////////////////////////////////////////
         virtual ~JSObject();
 
         virtual void *getEventId() { return NULL; }
         virtual void *getEventContext() { return NULL; }
 
+        ////////////////////////////////////////////////////////////////////////////////////////////////////
+        /// @fn virtual void :::InvokeAsync(const std::string& methodName, const std::vector<variant>& args)
+        ///
+        /// @brief  Just like Invoke, but works asynchronously.  Useful for javascript callbacks and events.
+        /// 		Can be safely called from any thread
+        ///
+        /// @param  methodName  Name of the method. 
+        /// @param  args        The arguments. 
+        /// @see Invoke
+        ////////////////////////////////////////////////////////////////////////////////////////////////////
         virtual void InvokeAsync(const std::string& methodName, const std::vector<variant>& args);
+
+        ////////////////////////////////////////////////////////////////////////////////////////////////////
+        /// @fn virtual void :::SetPropertyAsync(const std::string& propertyName, const variant& value)
+        ///
+        /// @brief  Just like SetProperty, but works asynchronously.  Useful if you are running on another
+        /// 		thread and don't need to wait to be sure it worked.
+        ///
+        /// @param  propertyName    Name of the property. 
+        /// @param  value           The value. 
+        ////////////////////////////////////////////////////////////////////////////////////////////////////
         virtual void SetPropertyAsync(const std::string& propertyName, const variant& value);
 
     public:
-        // TODO: Find a better place for this conversion method.
-        //       Gotcha to watch out for: has to be included after variant.h
-        //       and everywhere where variant::convert_cast<SomeContainer>()
-        //       is used.
+        ////////////////////////////////////////////////////////////////////////////////////////////////////
+        /// @fn template<class Cont> static void :::GetArrayValues(const FB::JSObjectPtr& src, Cont& dst)
+        ///
+        /// @brief  Gets Array values out of src and adds them to the STL container dst 
+        ///
+        /// @param  src         Source for the. 
+        /// @param [in,out] dst Destination for the. 
+        ///
+        /// ### todo    Find a better place for this conversion method. Gotcha to watch out for: has to
+        /// be included after variant.h and everywhere where variant::convert_cast<SomeContainer>()
+        /// is used. 
+        ////////////////////////////////////////////////////////////////////////////////////////////////////
         template<class Cont>
         static void GetArrayValues(const FB::JSObjectPtr& src, Cont& dst);
-        
+
+        ////////////////////////////////////////////////////////////////////////////////////////////////////
+        /// @fn template<class Dict> static void :::GetObjectValues(const FB::JSObjectPtr& src, Dict& dst)
+        ///
+        /// @brief  Gets object values out of the javscript object src and adds them to the STL Dict
+        /// 		container dst
+        ///
+        /// @param  src         Source for the. 
+        /// @param [in,out] dst Destination for the. 
+        ////////////////////////////////////////////////////////////////////////////////////////////////////
         template<class Dict>
         static void GetObjectValues(const FB::JSObjectPtr& src, Dict& dst);
 
