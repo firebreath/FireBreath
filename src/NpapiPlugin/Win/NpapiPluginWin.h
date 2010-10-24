823601d333fc968b39cb07ffeb96786f32d63095
Richard Bateman <taxilian@gmail.com>
2010-10-23 21:50:02 -0600

    Added some docs

--- src/NpapiPlugin/Win/NpapiPluginWin.h
+++ src/NpapiPlugin/Win/NpapiPluginWin.h
@@ -19,8 +19,6 @@ Copyright 2009 PacketPass, Inc and the Firebreath development team
 #include "Win/win_common.h"
 #include "NpapiPlugin.h"
 
-#include "Win/FactoryDefinitionsWin.h"
-
 namespace FB { 
     class PluginWindow;
     class PluginWindowWin;
