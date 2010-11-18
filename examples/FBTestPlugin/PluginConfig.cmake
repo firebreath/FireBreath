1f4503f14fd0cbfadbf272bb5bb23b8aac856535
Richard Bateman <taxilian@gmail.com>
2010-11-18 12:51:10 -0700

    Re-enabled all event models on mac for FBTestPlugin

--- examples/FBTestPlugin/PluginConfig.cmake
+++ examples/FBTestPlugin/PluginConfig.cmake
@@ -43,12 +43,10 @@ set(FBSTRING_MIMEType "application/x-fbtestplugin")
 # mac settings
 # The plugin doesn't do any drawing and doesn't need mouse / keyboard events,
 # so turn the following off:
-set (FBMAC_USE_CARBON        0)
+set (FBMAC_USE_CARBON        1)
 set (FBMAC_USE_COCOA         1)
-set (FBMAC_USE_QUICKDRAW     0)
+set (FBMAC_USE_QUICKDRAW     1)
 set (FBMAC_USE_COREGRAPHICS  1)
-set (FBMAC_USE_COREANIMATION 0)
+set (FBMAC_USE_COREANIMATION 1)
 
 add_firebreath_library(log4cplus)
-
-set (FBWIN_WINDOWLESS 1)
