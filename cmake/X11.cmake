abf518baef832e2169841247eaf8c6637ab25168
Richard Bateman <taxilian@gmail.com>
2010-12-12 00:08:35 -0700

    Fixed X11 compile problem

--- cmake/X11.cmake
+++ cmake/X11.cmake
@@ -29,22 +29,22 @@ set(BUILD_SHARED_LIBS YES)
 # which is not gcc compatible
 set(NPAPI_LINK_FLAGS "-Wl,--discard-all -Wl,-Bsymbolic -Wl,-z,defs -Wl,--version-script=${FB_ROOT_DIR}/gen_templates/version_script.txt")
 
-MACRO(add_x11_plugin PROJNAME INSOURCES)
-    # GTK is required for XEmbed to work
-    if (NOT FB_GUI_DISABLED)
-        if (NOT GTK_INCLUDE_DIRS)
-            pkg_check_modules(GTK REQUIRED gtk+-2.0)
-            set (GTK_INCLUDE_DIRS ${GTK_INCLUDE_DIRS} CACHE INTERNAL "GTK include dirs")
-            set (GTK_LIBRARIES ${GTK_LIBRARIES} CACHE INTERNAL "GTK include dirs")
-            set (GTK_LIBRARY_DIRS ${GTK_LIBRARY_DIRS} CACHE INTERNAL "GTK include dirs")
-            set (GTK_LDFLAGS ${GTK_LDFLAGS} CACHE INTERNAL "GTK include dirs")
-        endif()
-    else()
-        set (GTK_INCLUDE_DIRS "")
-        set (GTK_LIBRARIES "")
-        set (GTK_LDFLAGS "")
+# GTK is required for XEmbed to work
+if (NOT FB_GUI_DISABLED)
+    if (NOT GTK_INCLUDE_DIRS)
+        pkg_check_modules(GTK REQUIRED gtk+-2.0)
+        set (GTK_INCLUDE_DIRS ${GTK_INCLUDE_DIRS} CACHE INTERNAL "GTK include dirs")
+        set (GTK_LIBRARIES ${GTK_LIBRARIES} CACHE INTERNAL "GTK include dirs")
+        set (GTK_LIBRARY_DIRS ${GTK_LIBRARY_DIRS} CACHE INTERNAL "GTK include dirs")
+        set (GTK_LDFLAGS ${GTK_LDFLAGS} CACHE INTERNAL "GTK include dirs")
     endif()
+else()
+    set (GTK_INCLUDE_DIRS "")
+    set (GTK_LIBRARIES "")
+    set (GTK_LDFLAGS "")
+endif()
 
+MACRO(add_x11_plugin PROJNAME INSOURCES)
     add_definitions(
         -D"FB_X11=1"
     )
