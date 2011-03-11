615cc4add089a5ca1facb192cac8e0e37c9cef12
Kalev Lember <kalev@smartlink.ee>
2011-03-11 14:43:49 +0200

    Fix GCC -Wparentheses warning
    firebreath/src/ScriptingCore/utf8.c: In function ‘wchar_to_utf8’:
    firebreath/src/ScriptingCore/utf8.c:277:4: warning: suggest parentheses around arithmetic in operand of ‘|’ [-Wparentheses]
    firebreath/src/ScriptingCore/utf8.c:282:4: warning: suggest parentheses around arithmetic in operand of ‘|’ [-Wparentheses]
    firebreath/src/ScriptingCore/utf8.c:288:4: warning: suggest parentheses around arithmetic in operand of ‘|’ [-Wparentheses]
    firebreath/src/ScriptingCore/utf8.c:296:4: warning: suggest parentheses around arithmetic in operand of ‘|’ [-Wparentheses]
    firebreath/src/ScriptingCore/utf8.c:301:4: warning: suggest parentheses around arithmetic in operand of ‘|’ [-Wparentheses]
    firebreath/src/ScriptingCore/utf8.c:305:4: warning: suggest parentheses around arithmetic in operand of ‘|’ [-Wparentheses]
    firebreath/src/ScriptingCore/utf8.c:309:4: warning: suggest parentheses around arithmetic in operand of ‘|’ [-Wparentheses]

--- src/ScriptingCore/utf8.c
+++ src/ScriptingCore/utf8.c
@@ -274,18 +274,18 @@ wchar_to_utf8(const wchar_t *in, size_t insize, char *out, size_t outsize,
 			break;
 
 		case 2:
-			p[1] = _NXT | oc[3] & 0x3f;
+			p[1] = _NXT | (oc[3] & 0x3f);
 			p[0] = _SEQ2 | (oc[3] >> 6) | ((oc[2] & 0x07) << 2);
 			break;
 
 		case 3:
-			p[2] = _NXT | oc[3] & 0x3f;
+			p[2] = _NXT | (oc[3] & 0x3f);
 			p[1] = _NXT | (oc[3] >> 6) | ((oc[2] & 0x0f) << 2);
 			p[0] = _SEQ3 | ((oc[2] & 0xf0) >> 4);
 			break;
 
 		case 4:
-			p[3] = _NXT | oc[3] & 0x3f;
+			p[3] = _NXT | (oc[3] & 0x3f);
 			p[2] = _NXT | (oc[3] >> 6) | ((oc[2] & 0x0f) << 2);
 			p[1] = _NXT | ((oc[2] & 0xf0) >> 4) |
 			    ((oc[1] & 0x03) << 4);
@@ -293,20 +293,20 @@ wchar_to_utf8(const wchar_t *in, size_t insize, char *out, size_t outsize,
 			break;
 
 		case 5:
-			p[4] = _NXT | oc[3] & 0x3f;
+			p[4] = _NXT | (oc[3] & 0x3f);
 			p[3] = _NXT | (oc[3] >> 6) | ((oc[2] & 0x0f) << 2);
 			p[2] = _NXT | ((oc[2] & 0xf0) >> 4) |
 			    ((oc[1] & 0x03) << 4);
 			p[1] = _NXT | (oc[1] >> 2);
-			p[0] = _SEQ5 | oc[0] & 0x03;
+			p[0] = _SEQ5 | (oc[0] & 0x03);
 			break;
 
 		case 6:
-			p[5] = _NXT | oc[3] & 0x3f;
+			p[5] = _NXT | (oc[3] & 0x3f);
 			p[4] = _NXT | (oc[3] >> 6) | ((oc[2] & 0x0f) << 2);
 			p[3] = _NXT | (oc[2] >> 4) | ((oc[1] & 0x03) << 4);
 			p[2] = _NXT | (oc[1] >> 2);
-			p[1] = _NXT | oc[0] & 0x3f;
+			p[1] = _NXT | (oc[0] & 0x3f);
 			p[0] = _SEQ6 | ((oc[0] & 0x40) >> 6);
 			break;
 		}
