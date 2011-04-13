8b32161868a84b893c8b17070c791664faffd3bb
Richard Bateman <taxilian@gmail.com>
2011-04-13 15:20:04 -0600

    FIREBREATH-41 FB_PROJECTS_DIR can be a single project

--- cmake/options.cmake
+++ cmake/options.cmake
@@ -1,7 +1,11 @@
 # options which affect the way Firebreath is built
 
-set(FB_PROJECTS_DIR "${FB_ROOT}/projects" CACHE STRING "Location to search for project directories")
-set(FB_CONFIG_DIR "${FB_ROOT}/src/config" CACHE STRING "Location to find global project configuration files (.h files, etc)" )
+if (NOT FB_PROJECTS_DIR)
+    set(FB_PROJECTS_DIR "${FB_ROOT}/projects" CACHE STRING "Location to search for project directories")
+endif()
+if (NOT FB_CONFIG_DIR)
+    set(FB_CONFIG_DIR "${FB_ROOT}/src/config" CACHE STRING "Location to find global project configuration files (.h files, etc)" )
+endif()
 
 option(BUILD_EXAMPLES "Build example projects" OFF)
 option(WITH_DYNAMIC_MSVC_RUNTIME "Build with dynamic MSVC runtime (/MD)" OFF)
