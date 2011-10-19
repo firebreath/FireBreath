36b8402da0fd867e59fac3bf3c7d89e55eafb7ca
Max Amanshauser <amx.NOSPAM@lambdalifting.org>
2011-10-19 23:13:04 +0200

    Removed unnecessary main(.) to allow building with clang.

--- src/PluginAuto/Mac/np_macmain.cpp
+++ src/PluginAuto/Mac/np_macmain.cpp
@@ -20,24 +20,9 @@ extern "C" {
     NPError NP_Initialize(NPNetscapeFuncs *browserFuncs);
     NPError NP_GetEntryPoints(NPPluginFuncs *pluginFuncs);
     NPError NP_Shutdown(void);
-
-#ifndef _NO_MAIN
-    // For compatibility with CFM browsers.
-    int main(NPNetscapeFuncs *browserFuncs, NPPluginFuncs *pluginFuncs, NPP_ShutdownProcPtr *shutdown);
-#endif
 }
 
 #pragma GCC visibility pop
-
-#ifndef _NO_MAIN
-int main(NPNetscapeFuncs *browserFuncs, NPPluginFuncs *pluginFuncs, NPP_ShutdownProcPtr *shutdown)
-{
-    FBLOG_TRACE("PluginCore", __func__);
-    NP_Initialize(browserFuncs);
-    NP_GetEntryPoints(pluginFuncs);
-    *shutdown = (NPP_ShutdownProcPtr)&NP_Shutdown;
-}
-#endif
 
 void initPluginModule()
 {
@@ -71,4 +56,3 @@ NPError OSCALL NP_Shutdown()
     module = NULL;
     return NPERR_NO_ERROR;
 }
-
