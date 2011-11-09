da1edb110dc771a9fcc3893728c360a7167e87c6
Max Amanshauser <amx.NOSPAM@lambdalifting.org>
2011-11-09 19:43:53 +0100

    Added logging macros that take a single argument.

--- src/PluginAuto/log4cplus/log4cplus.cpp
+++ src/PluginAuto/log4cplus/log4cplus.cpp
@@ -91,6 +91,31 @@ void FB::Log::stopLogging()
     logging_started = false;
 }
 
+void FB::Log::trace(const std::string& msg, const char *file, int line, const char *fn)
+{
+    trace("",msg,file,line,fn);
+}
+void FB::Log::debug(const std::string& msg, const char *file, int line, const char *fn)
+{
+    debug("",msg,file,line,fn);
+}
+void FB::Log::info(const std::string& msg, const char *file, int line, const char *fn)
+{
+    info("",msg,file,line,fn);
+}
+void FB::Log::warn(const std::string& msg, const char *file, int line, const char *fn)
+{
+    warn("",msg,file,line,fn);
+}
+void FB::Log::error(const std::string& msg, const char *file, int line, const char *fn)
+{
+    error("",msg,file,line,fn);
+}
+void FB::Log::fatal(const std::string& msg, const char *file, int line, const char *fn)
+{
+    fatal("",msg,file,line,fn);
+}
+
 void FB::Log::trace(const std::string& src, const std::string& msg, const char *file, int line, const char *fn)
 {
     LOG4CPLUS_TRACE(log4cplus::Logger::getInstance(L"FireBreath"), 
@@ -121,4 +146,3 @@ void FB::Log::fatal(const std::string& src, const std::string& msg, const char *
     LOG4CPLUS_ERROR(log4cplus::Logger::getInstance(L"FireBreath"), 
                     file << ":" << line << " - " << fn << " - " << FB::utf8_to_wstring(msg));
 }
-
