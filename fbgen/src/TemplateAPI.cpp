7fc0f17c58e4e1291e725442071024fedfc2747e
Max Amanshauser <amx.NOSPAM@lambdalifting.org>
2012-02-08 00:16:48 +0100

    Improved FBControl.htm and default API a bit.

--- fbgen/src/TemplateAPI.cpp
+++ fbgen/src/TemplateAPI.cpp
@@ -12,41 +12,18 @@
 #include "@{PLUGIN_ident}API.h"
 
 ///////////////////////////////////////////////////////////////////////////////
-/// @@fn @{PLUGIN_ident}API::@{PLUGIN_ident}API(const @{PLUGIN_ident}Ptr& plugin, const FB::BrowserHostPtr host)
+/// @@fn FB::variant @{PLUGIN_ident}API::echo(const FB::variant& msg)
 ///
-/// @@brief  Constructor for your JSAPI object.  You should register your methods, properties, and events
-///         that should be accessible to Javascript from here.
-///
-/// @@see FB::JSAPIAuto::registerMethod
-/// @@see FB::JSAPIAuto::registerProperty
-/// @@see FB::JSAPIAuto::registerEvent
+/// @@brief  Echos whatever is passed from Javascript.
+///         Go ahead and change it. See what happens!
 ///////////////////////////////////////////////////////////////////////////////
-@{PLUGIN_ident}API::@{PLUGIN_ident}API(const @{PLUGIN_ident}Ptr& plugin, const FB::BrowserHostPtr& host) : m_plugin(plugin), m_host(host)
+FB::variant @{PLUGIN_ident}API::echo(const FB::variant& msg)
 {
-    registerMethod("echo",      make_method(this, &@{PLUGIN_ident}API::echo));
-    registerMethod("testEvent", make_method(this, &@{PLUGIN_ident}API::testEvent));
+    static int n(0);
+    fire_echo("So far, you clicked this many times: ", n++);
 
-    // Read-write property
-    registerProperty("testString",
-                     make_property(this,
-                        &@{PLUGIN_ident}API::get_testString,
-                        &@{PLUGIN_ident}API::set_testString));
-
-    // Read-only property
-    registerProperty("version",
-                     make_property(this,
-                        &@{PLUGIN_ident}API::get_version));
-}
-
-///////////////////////////////////////////////////////////////////////////////
-/// @@fn @{PLUGIN_ident}API::~@{PLUGIN_ident}API()
-///
-/// @@brief  Destructor.  Remember that this object will not be released until
-///         the browser is done with it; this will almost definitely be after
-///         the plugin is released.
-///////////////////////////////////////////////////////////////////////////////
-@{PLUGIN_ident}API::~@{PLUGIN_ident}API()
-{
+    // return "foobar";
+    return msg;
 }
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -66,13 +43,12 @@
     return plugin;
 }
 
-
-
 // Read/Write property testString
 std::string @{PLUGIN_ident}API::get_testString()
 {
     return m_testString;
 }
+
 void @{PLUGIN_ident}API::set_testString(const std::string& val)
 {
     m_testString = val;
@@ -84,16 +60,7 @@ std::string @{PLUGIN_ident}API::get_version()
     return FBSTRING_PLUGIN_VERSION;
 }
 
-// Method echo
-FB::variant @{PLUGIN_ident}API::echo(const FB::variant& msg)
+void @{PLUGIN_ident}API::testEvent()
 {
-    static int n(0);
-    fire_echo(msg, n++);
-    return msg;
+    fire_test();
 }
-
-void @{PLUGIN_ident}API::testEvent(const FB::variant& var)
-{
-    fire_fired(var, true, 1);
-}
-
