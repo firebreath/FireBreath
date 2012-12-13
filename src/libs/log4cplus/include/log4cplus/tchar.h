504cc8a26fd0142c21d7a520403083954456a9f0
Richard Bateman <taxilian@gmail.com>
2012-12-13 16:11:44 -0700

    Remove debugging code that should have been local only

--- src/libs/log4cplus/include/log4cplus/tchar.h
+++ src/libs/log4cplus/include/log4cplus/tchar.h
@@ -1,16 +1,16 @@
 // -*- C++ -*-
 //  Copyright (C) 2010, Vaclav Haisman. All rights reserved.
-//  
+//
 //  Redistribution and use in source and binary forms, with or without modifica-
 //  tion, are permitted provided that the following conditions are met:
-//  
+//
 //  1. Redistributions of  source code must  retain the above copyright  notice,
 //     this list of conditions and the following disclaimer.
-//  
+//
 //  2. Redistributions in binary form must reproduce the above copyright notice,
 //     this list of conditions and the following disclaimer in the documentation
 //     and/or other materials provided with the distribution.
-//  
+//
 //  THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED WARRANTIES,
 //  INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
 //  FITNESS  FOR A PARTICULAR  PURPOSE ARE  DISCLAIMED.  IN NO  EVENT SHALL  THE
@@ -53,8 +53,6 @@ namespace log4cplus
 typedef wchar_t tchar;
 #else
 typedef char tchar;
-    dsfsdafdsafds
-
 #endif
 
 } // namespace log4cplus
