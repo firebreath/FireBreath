7370f9e93fb24feaa9add4358b65ae716a284005
Michael FiG <git@michael.fig.org>
2013-07-25 20:21:20 -0600

    Portability fixes.

--- cmake/xpi.cmake
+++ cmake/xpi.cmake
@@ -15,19 +15,27 @@ function (add_signed_xpi_installer PROJNAME XPI_OUTDIR DLLFILE XPISIGNERPATH PFX
 			file(STRINGS "${PASSFILE}" PASSPHRASE LIMIT_COUNT 1)
         endif()
 
+			if ("${XPISIGNERPATH}" STREQUAL "")
+			  set(SIGN_COMMAND cd "${XPI_OUTDIR}/${FBSTRING_PluginFileName}" && rm -f "../${FBSTRING_PluginFileName}.xpi" && zip -r "../${FBSTRING_PluginFileName}.xpi" *)
+			else()	  
+			  set(SIGN_COMMAND java -jar ${XPISIGNERPATH} ${PFXFILE} ${PASSPHRASE} "${XPI_OUTDIR}/${FBSTRING_PluginFileName}" "${XPI_OUTDIR}/${FBSTRING_PluginFileName}.xpi")
+			endif()
+
 		#Replace the plugin version in the install.rdf template
 		file(MAKE_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/gen/${FBSTRING_PluginFileName}-xpi)
-		file(COPY ${CMAKE_CURRENT_SOURCE_DIR}/xpi/content/chrome.manifest DESTINATION ${CMAKE_CURRENT_BINARY_DIR}/gen/${FBSTRING_PluginFileName}-xpi)
+		if (EXISTS ${CMAKE_CURRENT_SOURCE_DIR}/xpi/content/chrome.manifest)
+		  file(COPY ${CMAKE_CURRENT_SOURCE_DIR}/xpi/content/chrome.manifest DESTINATION ${CMAKE_CURRENT_BINARY_DIR}/gen/${FBSTRING_PluginFileName}-xpi)
+		endif()
 		configure_template(${CMAKE_CURRENT_SOURCE_DIR}/xpi/content/install.rdf ${CMAKE_CURRENT_BINARY_DIR}/gen/${FBSTRING_PluginFileName}-xpi/install.rdf)
 
 		add_custom_command(
 			TARGET ${PROJNAME}${FB_XPI_SUFFIX}
 			POST_BUILD
+			COMMAND ${CMAKE_COMMAND} -E remove_directory "${XPI_OUTDIR}/${FBSTRING_PluginFileName}"
 			COMMAND ${CMAKE_COMMAND} -E copy_directory "\"${CMAKE_CURRENT_BINARY_DIR}/gen/${FBSTRING_PluginFileName}-xpi\"" "\"${XPI_OUTDIR}/${FBSTRING_PluginFileName}/\""
 			COMMAND mkdir "\"${XPI_OUTDIR}/${FBSTRING_PluginFileName}/plugins/\""
 			COMMAND ${CMAKE_COMMAND} -E copy "${DLLFILE}" "\"${XPI_OUTDIR}/${FBSTRING_PluginFileName}/plugins/\""
-			COMMAND java -jar ${XPISIGNERPATH} ${PFXFILE} ${PASSPHRASE} "${XPI_OUTDIR}/${FBSTRING_PluginFileName}" "${XPI_OUTDIR}/${FBSTRING_PluginFileName}.xpi"
-			COMMAND popd
+			COMMAND ${SIGN_COMMAND}
 			COMMAND ${CMAKE_COMMAND} -E remove_directory "${XPI_OUTDIR}/${FBSTRING_PluginFileName}"
 		 )
         ADD_DEPENDENCIES(${PROJNAME}${FB_XPI_SUFFIX} ${XPI_PROJDEP})
