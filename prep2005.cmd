8b01c8f322290fa6322e45486781383ddb0e9f67
Richard Bateman <taxilian@gmail.com>
2015-03-06 09:15:01 -0700

    Remove prep scripts for vs < 2010

--- prep2005.cmd
+++
@@ -1,7 +0,0 @@
-@echo off & setlocal enableextensions enabledelayedexpansion
-
-set _FB_GEN="Visual Studio 8 2005"
-
-call "%~d0%~p0\common.cmd" %*
-if %errorlevel% == 2 exit /b 1
-call "%~d0%~p0\winprep.cmd"
--- prep2008.cmd
+++
@@ -1,7 +0,0 @@
-@echo off & setlocal enableextensions enabledelayedexpansion
-
-set _FB_GEN="Visual Studio 9 2008"
-
-call "%~d0%~p0\common.cmd" %*
-if %errorlevel% == 2 exit /b 1
-call "%~d0%~p0\winprep.cmd"
