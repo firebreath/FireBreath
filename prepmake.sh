edf4c7bfdf8d3b48efd43c1868a666c83325d188
Nikita Bige <n.bige@rutoken.ru>
2015-05-07 16:34:10 +0300

    we can build examples in linux, now

--- prepmake.sh
+++ prepmake.sh
@@ -5,7 +5,16 @@ if [ "${GEN}" = "" ]; then
 fi
 
 source ${0%/*}/common.sh "$@"
+shift $USED_ARGS
 
 pushd "$BUILDDIR"
-cmake -G "$GEN" -DFB_PROJECTS_DIR="${PROJDIR}" "$@" "${FB_ROOT}"
-popd
+echo cmake -G "$GEN" -DFB_ROOT="${FB_ROOT}" "$@" "${PROJDIR}"
+#cmake -G "$GEN" -DFB_PROJECTS_DIR="${PROJDIR}" "$@" "${FB_ROOT}"
+cmake -G "$GEN" -DFB_ROOT="${FB_ROOT}" "$@" "${PROJDIR}"
+if [ "$?" -ne 0 ]; then
+    echo "CMake failed. Please check error messages"
+    popd > /dev/null
+    exit
+else
+    popd
+fi
