590fcd991851c9ee4872911b18db4f3fe635eac8
brainysmith <brainysmith@gmail.com>
2016-01-18 12:47:18 +0300

    Added old utf8 converter because in Linux's C++ stdlib the is no codecvt.

--- src/ScriptingCore/utf8_tools.cpp
+++ src/ScriptingCore/utf8_tools.cpp
@@ -18,21 +18,42 @@ Copyright 2009 Dan Weatherford, Facebook inc
 #include <limits.h>
 #include <boost/algorithm/string/case_conv.hpp>
 #include <locale>
+#ifdef __linux
+#include "../3rdParty/utf8/utf8.h"
+#else 
 #include <codecvt>
+#endif
 
 #include "utf8_tools.h"
 
+#ifndef __linux 
 std::wstring_convert<std::codecvt_utf8<wchar_t>> utf8_conv;
+#endif
 
 namespace FB {
 
-    std::string wstring_to_utf8(const std::wstring& src) {
+    std::string wstring_to_utf8(const std::wstring& src) { 
+#ifdef __linux
+        std::string out_str;
+        std::wstring in_str;
+        utf8::replace_invalid(src.begin(), src.end(), std::back_inserter(in_str));
+        utf8::utf32to8(in_str.begin(), in_str.end(), std::back_inserter(out_str));
+        return out_str;
+#else
         return utf8_conv.to_bytes(src);
+#endif
     }
-
-
+    
     std::wstring utf8_to_wstring(std::string src) {
+#ifdef __linux
+        std::wstring out_str;
+        std::string in_str;
+        utf8::replace_invalid(src.begin(), src.end(), std::back_inserter(in_str));
+        utf8::utf8to32(in_str.begin(), in_str.end(), std::back_inserter(out_str));
+        return out_str;
+#else
         return utf8_conv.from_bytes(src);
+#endif
     }
 
 
