7667547f1cbc0623c1a23a2910c0eae55656401d
Wojciech Wałek <wojciech.walek@gmail.com>
2017-02-02 12:11:08 +0100

    Fixing #184 pull-request changes - calling Invoke twice and using FB:variant instead of FB::variantPromise

--- src/ActiveXCore/JSAPI_IDispatchEx.h
+++ src/ActiveXCore/JSAPI_IDispatchEx.h
@@ -395,11 +395,11 @@ namespace FB { namespace ActiveX {
                         params.emplace_back(host->getVariant(&pdp->rgvarg[i]));
                     }
                 }
-                FB::variant rVal;
+                FB::variantPromise rVal;
                 rVal = api->Invoke(wsName, params);
 		
                 if(pvarRes)
-			setPromise(api->Invoke(wsName, params), pvarRes);
+                    setPromise(rVal, pvarRes);
                 
             } else if (wFlags & DISPATCH_PROPERTYGET && api->HasMethod(wsName)) {
                 
